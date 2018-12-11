5c0d6fb907be958d899796eb15e0dd6b64d4bf65
Jack Brookes <45665939+jbsaggezza@users.noreply.github.com>
2018-12-11 14:26:34 +0000

    Update AlgorithmPractice.java

--- AlgorithmPractice.java
+++ AlgorithmPractice.java
@@ -13,8 +13,10 @@ public class AlgorithmPractice {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int [] testArr = {10,2,88,71,69,56,44,31,23};
+         int [] testArr = {10,2,88,71,69,56,44,31,23};
         insertionSort(testArr);
+        
+        intersectionDemo();
     }
     
     static void insertionSort(int[] a){
@@ -35,6 +37,79 @@ public class AlgorithmPractice {
             
         }
         System.out.println(Arrays.toString(a));
+    }
+    
+    static void unionDemo(){
+        
+        Set <Integer> s1 = new HashSet <>();
+        Set <Integer> s2 = new HashSet <>();
+        s1.add(13);
+        s1.add(20);
+        s1.add(2);
+        s1.add(40);
+        s1.add(51);
+        
+        s2.add(1);
+        s2.add(4);
+        s2.add(5);
+        s2.add(78);
+        
+        Set <Integer> union = new HashSet <>();
+        
+        for(int i: s1){
+            
+            union.add(i);        
+        }
+        
+        for(int i: s2){
+            
+            union.add(i);        
+        }
+        
+        for(int i : union){
+            
+            System.out.println(i);
+        }
+    }
+    
+    static void intersectionDemo(){
+        
+        Set <Integer> s1 = new HashSet <>();
+        Set <Integer> s2 = new HashSet <>();
+        Set <Integer> smallerSet = new HashSet <>();
+        Set <Integer> intersection = new HashSet <>();
+        
+        s1.add(1);
+        s1.add(20);
+        s1.add(2);
+        s1.add(40);
+        s1.add(51);
+        
+        s2.add(1);
+        s2.add(4);
+        s2.add(5);
+        s2.add(2);
+        
+        if(s1.size() < s2.size()){
+            
+            smallerSet = s2;
+        } 
+        
+         if(s1.size() > s2.size()){
+            
+            smallerSet = s2;
+        } 
+          
+        for(int i: smallerSet){
+            if(s1.contains(i) && s2.contains(i)){
+            intersection.add(i); 
+            }
+        }
+        
+        for(int i : intersection){
+            
+            System.out.println(i);
+        }  
     }
     
 }
